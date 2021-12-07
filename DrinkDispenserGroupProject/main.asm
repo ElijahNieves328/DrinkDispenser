bbbd8ed294b2d067084a1bd07a29e41393f84003
Elijah Nieves <elijahnieves328@gmail.com>
2021-12-07 15:49:12 -0500

    Initial Commit

---
+++ DrinkDispenserGroupProject/main.asm
@@ -0,0 +1,17 @@
+;
+; DrinkDispenserGroupProject.asm
+;
+; Created: 12/7/2021 3:35:16 PM
+; Author : Elijah Nieves
+;
+
+main:
+	ldi r16, high(RAMEND)
+	out SPH, r16
+	ldi r16, low(RAMEND)
+	out SPL, r16
+
+
+
+end_main:
+	rjmp end_main
