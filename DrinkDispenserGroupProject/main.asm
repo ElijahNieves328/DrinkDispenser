285e9c7b168b55a4727db43b5a1b819c44c3822f
Elijah Nieves <elijahnieves328@gmail.com>
2021-12-07 21:41:30 -0500

    Added a timer and the basic routine for drink dispensing.
    Added:
    Comments explaining the program and a TODO list
    3 Second timer
    Wait routine which waits for input
    Drink dispensing routine that spins servo until the button (right button) is not pressed or the water sensor says there's no liquid
    A routine to set the "Out of Liquid" LED.
    A routine to blink the LED if the drink button is pressed but there is not sufficient liquid

--- DrinkDispenserGroupProject/main.asm
+++ DrinkDispenserGroupProject/main.asm
@@ -4,6 +4,19 @@
 ; Created: 12/7/2021 3:35:16 PM
 ; Author : Elijah Nieves, Robert McNiven, Michael D'Agostino
 ;
+; This program is made to function as a drink dispenser. When the right button is pressed, it will dispense liquid (this is simulated by turning a servo)
+; However, there is a water sensor to detect how much liquid is left. If there is not a sufficient amount of liquid, the program will stop dispensing and turn on an LED which signals the lack of liquid. 
+; If the drink dispensing button is pressed again while there is not sufficient liquid, it will blink the LED to tell the user there is not sufficient liquid.
+;
+; Additionally, the left button is used to simulate dispensing ice. When the button is pressed, it will spin the servo for 3 seconds. This is to represent the machine dispensing a couple of ice cubes. 
+
+; TODO: 
+	; Make sure the algorithm is correct by running it on the Arduino set up
+	; Make sure the program uses the water sensor input properly
+	; Change the 3 second timer to a 1 second timer
+	; Implement the ice cube routine (maybe as an interrupt?)
+	;
+
 
 .equ Right_LED = PB1
 .equ Left_LED = PD6
@@ -29,11 +42,79 @@ main:
 
 	cbi DDRC, DDC3		; clears the Water Sensor pin in the direction register C for input
 
-	sbi PORTD, Right_Button		; set Right button output pin in PORTD so that Right Button is set for pull up
-	sbi PORTD, Left_Button		; set Left button output pin in PORTD so that Left Button is set for pull up
+	sbi PORTD, Right_Button			; set Right button output pin in PORTD so that Right Button is set for pull up
+	sbi PORTD, Left_Button			; set Left button output pin in PORTD so that Left Button is set for pull up
 
-	sbi PORTC, Water_Level_Sensor		; set Water Sensor output pin in PORTD so that Water Sensor is set for pull up
+	sbi PORTC, Water_Level_Sensor	; set Water Sensor output pin in PORTD so that Water Sensor is set for pull up
+
+	; make sure the LEDs are off
+	cbi PORTB, Right_LED		
+	cbi PORTD, Left_LED
+
+Wait:
+	sbis PIND, Right_Button			; unless there is input (in pull-up, input = 1), skip the right input routine
+	call RightInput					; call the routuine for the right button being pressed
+	cbi PORTD, Servo				; turn off the Servo if it is on
+	rjmp Wait
+
+RightInput:
+	sbic PINC, Water_Level_Sensor   ; if the sensor is not sending input (it is dry)
+	rjmp BlinkRightLEDTwice			; blink the LED instead of dispensing, which then returns to the main routine
+	sbi PORTD, Servo				; else, turn on the servo
+DispenseDrink:
+	sbis PIND, Right_Button			; if the Right button is not being pressed
+	ret								; return the the main routine where it turns off the Servo
+	
+	sbic PINC, Water_Level_Sensor   ; if the sensor is not sending input (it is dry)
+	rjmp OutofLiquid				; stop dispensing liquid and run the out of liquid routine
+
+	rjmp DispenseDrink				; else, keep dispensing drink
+
+OutofLiquid:
+	cbi PORTD, Servo				; turn off the servo
+	sbi PORTB, Right_LED			; turn on the Right LED to signal that there is no more Right Drink.
+	ret								; return to main routine
 
 
-end_main:
-	rjmp end_main
+BlinkRightLEDTwice:
+	ldi r21, 2						; this is how many times we have blinked
+
+	sbi PORTB, Right_LED			; turn on the Right LED
+	call T1Normal_3sec				; change this later to be a 1 second delay
+	cbi PORTB, Right_LED			; turns off the Right LED
+
+	dec r21
+	brne OutOfLiquid				; when r21 == 2, we have blinked twice and will exit.
+	rjmp blink						; loop of one second blinking
+
+
+; TODO: make this a 1 second timer instead
+T1Normal_3sec:
+; use this for the ice cubes! when ice cube button is pressed, turn the servo on, call the timer (call 3 times if the timer is 1 second), then turn the servo off.
+; 3,000,000 microsecond delay. (3 seconds)
+; Prescaler = 1024
+; Load TCNT1H:TCNT1L with initial count
+; TCNT1 = 18661 = $48:$E5
+	ldi r20, $48
+	sts TCNT1H, r20
+	ldi r20, $E5
+	sts TCNT1L, r20
+
+; Load TCCR1A & TCCR1B
+	clr r20
+	sts TCCR1A, r20
+	ldi r20, $05	
+	sts TCCR1B, r20		; Normal mode – clears all WGM, sets CS12 and CS10 (prescaler 1024)
+
+T1Normal_Wait:
+; Monitor TOV1 in TIFR1
+	sbis TIFR1, TOV1	; check to see if overflow flag is set(0?). if it is...
+	rjmp T1Normal_Wait
+
+	clr r20
+	sts TCCR1B, r20		; stop the timer by clearing it
+
+	sbi TIFR1, TOV1		; Clear TOV0 flag by writing a 1 to TOV0 bit in TIFR0
+
+	ret
+
